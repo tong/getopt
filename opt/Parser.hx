163b1b13892bfe2d80cf31ae27407e7138d62a64
tong <tong@disktree.net>
2009-11-23 16:59:57 +0100

    Initial import.

---
+++ opt/Parser.hx
@@ -0,0 +1,261 @@
+package opt;
+
+#if php
+import php.Sys;
+import php.Lib;
+#elseif neko
+import neko.Sys;
+import neko.Lib;
+#elseif cpp
+import cpp.Sys;
+import cpp.Lib;
+#end
+
+/**
+	Type of actions.
+*/
+enum Action {
+	storeTrue( dest : String );
+	storeFalse( dest : String );
+	store( dest : String, ?type : ArgumentType );
+	call( f : Void->Void );
+}
+
+/**
+	Argument datatypes.
+*/
+enum ArgumentType {
+	TInt;
+	TFloat;
+	TString;
+	TBool;
+}
+
+private typedef TAction = {
+	var switches : Array<String>;
+	var type : Action;
+	var help : String;
+	//var meta
+}
+
+private class Options implements Dynamic {
+	//public var unkown
+	public function new() {
+	}
+}
+
+/**
+	Getopt style terminal parser.
+*/
+class Parser {
+	
+	public var program : String;
+	public var opt(default,null) : Options;
+	public var actions : List<TAction>;
+	public var exe : String;
+	public var description : String;
+	
+	var args : Array<String>;
+	var i : Int;
+	
+	public function new( program : String ) {
+		this.program = program;
+		opt = new Options();
+		actions = new List();
+	}
+	
+	/**
+	*/
+	public function help() {
+		var b = new StringBuf();
+		b.add( "\n\nUsage = " );
+		if( exe != null ) {
+			b.add( exe );
+			b.add( " " );
+		}
+		if( program != null )
+			b.add( program );
+		if( actions.length > 0 ) {
+			b.add( " [Options]\n" );
+			b.add( "\nOptions:\n\n" );
+			b.add( "\t-h,--h\n\t\tShow this help message and exit\n" );
+			for( a in actions ) {
+				b.add( "\n\t" );
+				for( i in 0...a.switches.length ) {
+					b.add( a.switches[i] );
+					if( i != a.switches.length-1 )
+						b.add( ", " );
+				}
+				b.add( "\n" );
+				//if( a.switches.length > 1 )
+					//b.add( "\n" );
+				b.add( "\t\t" );
+				if( a.help != null ) {
+					b.add( a.help );
+				} else {
+					b.add( "undocumented." );
+				}
+				b.add( "\n" );
+			}
+		}
+		b.add( "\n" );
+		if( description != null ) {
+			b.add( description );
+			b.add( "\n" );
+		}
+		b.add( "\n" );
+		Lib.print( b.toString() );
+		Sys.exit( 0 );
+	}
+	
+	/**
+	*/
+	public function addOption( switches : Array<String>, action : Action, ?help : String ) : Bool {
+		for( ns in switches ) {
+			if( hasSwitch( ns) )
+				return false;
+		}
+		actions.push( { switches : switches, type : action, help : help } );
+		return true;
+	}
+	
+	/**
+	*/
+	public function clear() {
+		actions = new List();
+		opt = new Options();
+		i = 0;
+	}
+	
+	/**
+	*/
+	public function parseString( t : String ) {
+		t = StringTools.trim( t );
+		t = ~/(\s+)/.replace( t, " " );
+		parse( t.split( " " ) );
+	}
+	
+	/**
+	*/
+	public function parse( args : Array<String> ) {
+		this.args = args;
+		opt = new Options();
+		i = 0;
+		parseArgument();
+	}
+	
+	function parseArgument() {
+		var arg = args[i];	
+		switch( arg ) {
+		case null :
+			return;
+		default :
+			// check fot switches
+			var reg = ~/--?([a-zA-Z0-9_-]+)(\s*=\s*([a-zA-Z0-9_-]+))?/;
+			// check if '='
+			if( reg.match( arg ) ) {
+				if( reg.matched( 2 ) != null ) {
+					args[i] = reg.matched( 1 );
+					args.insert( i+1, reg.matched( 3 ) );
+					if( i > 0 ) {
+						i--;
+						arg = args[i];
+					}
+				}
+				var sw = reg.matched( 1 );
+				for( a in actions ) {
+					for( s in a.switches ) {
+						var sw1 = sw.substr( 0, s.length );
+						var sw2 = sw.substr( s.length );
+						if( s == sw1 ) { // switch match
+							if( sw2 != "" ) {
+								args[i] = sw1;
+								args.insert( i+1, sw2 );
+							}
+							resolveSwitch( sw, a.type );
+							i++;
+							continue;
+						}
+					}
+				}
+				//trace("UNHANDLED SWITCH VALUE "+sw);
+				addUnregisteredValue( sw );
+			} else {
+				//trace(" UNHANDLED VALUE "+arg);
+				addUnregisteredValue( arg, arg );
+			}
+		}
+		if( i < args.length-1 ) {
+			i++;
+			parseArgument();
+		}
+	}
+	
+	function addUnregisteredValue( arg : String, value : Dynamic = true ) {
+		if( hasSwitch( arg ) ) return;
+		Reflect.setField( opt, arg, value );
+	}
+	
+	function hasSwitch( t : String ) : Bool {
+		for( a in actions ) {
+			for( s in a.switches ) {
+				if( s == t )
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	function resolveSwitch( s : String, a : Action ) {
+		switch( a ) {
+		case storeTrue(d) :
+			Reflect.setField( opt, d, true );
+			return;
+		case storeFalse(d) :
+			Reflect.setField( opt, d, false );
+			return;
+		case store(d,t) :
+			if( t == null ) {
+				trace("NUUUUUUUUUUUUUUUULLLLLLLLLL TYPE " );
+				Reflect.setField( opt, d, s );
+				return;
+			}
+			var v : String = null;
+			if( t != TBool ) {
+				//TODO throw errors ?
+				if( i == args.length-1 )
+					throw "Missing ("+t+") value for switch: "+s ;
+				if( StringTools.startsWith( args[i+1], "-" ) )
+					throw "Missing argument value for: "+s;
+				v = args[i+1];
+				//TODO
+				v = ~/'(.+)'/g.replace( v, "$1" ); //unquote
+			}
+			switch( t ) {
+			case TInt :
+				Reflect.setField( opt, d, Std.parseInt( v ) );
+			case TFloat :
+				Reflect.setField( opt, d, Std.parseFloat( v ) );
+			case TString :
+				Reflect.setField( opt, d, v );
+			case TBool :
+				Reflect.setField( opt, d, true );
+			}
+		case call(cb) :
+			cb();
+		}
+		
+	}
+	
+	/*
+	public static function get( t : String, matrix : String ) : Dynamic {
+		var p = new Parser( null );
+		//p.parse();
+	}
+	
+	public static function getSysArgs( t : String ) : Dynamic {
+		return null;
+	}
+	*/
+	
+}
---
+++ test/TestOptParser.hx
@@ -0,0 +1,125 @@
+package test;
+
+import opt.Parser;
+
+class TestOptParser extends haxe.unit.TestCase {
+	
+	function test() {
+		
+		var terminal = new opt.Parser( "TestOptParser.n" );
+		terminal.addOption( ["f"], store( "filename", TString ) );
+		
+		terminal.parse( ["-f","myfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parse( ["--f","myfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+
+		terminal.parse( ["-f=myfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parse( ["--f=myfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parse( ["-f = myfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parse( ["-f	=	myfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parse( ["-fmyfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parse( ["--fmyfile"] );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parseString( "-fmyfile" );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parseString( "-f myfile" );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		terminal.parseString( " -f myfile " );
+		assertEquals( "myfile", terminal.opt.filename );
+		
+		
+		terminal = new opt.Parser( "TestOptParser.n" );
+		terminal.addOption( ["s"], store( "another", TInt ) );
+		
+		terminal.parseString( "-s 23" );
+		assertEquals( 23, terminal.opt.another );
+
+		terminal.parseString( "-s 23.4" );
+		assertEquals( 23, terminal.opt.another );
+		
+		terminal.parseString( "-s 23.9" );
+		assertEquals( 23, terminal.opt.another );
+		
+		terminal.parseString( "-s 24" );
+		assertFalse( terminal.opt.another==23 );
+		
+		//trace( Type.typeof(terminal.opt.another) );
+		//assertEquals(TInt, Type.typeof(terminal.opt.another) );
+		
+		/*
+		<yourscript> -f outfile --quiet
+		<yourscript> --quiet --file outfile
+		<yourscript> -q -foutfile
+		<yourscript> -qfoutfile
+		*/
+		terminal = new opt.Parser( "TestOptParser.n" );
+		
+		terminal.parseString( "-f outfile --quiet" );
+		assertEquals( true, terminal.opt.f );
+		assertEquals( true, terminal.opt.quiet );
+		assertEquals( "outfile", terminal.opt.outfile );
+		
+		terminal.parseString( "--quiet --file outfile" );
+		assertEquals( true, terminal.opt.file );
+		assertEquals( true, terminal.opt.quiet );
+		assertEquals( "outfile", terminal.opt.outfile );
+		
+		terminal.parseString( "--quiet -foutfile" );
+		assertEquals( null, terminal.opt.file );
+		assertEquals( true, terminal.opt.quiet );
+		assertEquals( null, terminal.opt.outfile );
+		assertEquals( true, terminal.opt.foutfile );
+		
+		terminal.addOption( ["f"], store( "filename", TString ) );
+		terminal.parseString( "--quiet -foutfile" );
+		assertEquals( true, terminal.opt.quiet );
+		assertEquals( "outfile", terminal.opt.filename );
+		assertEquals( null, terminal.opt.outfile );
+		
+		terminal.parseString( "-f outfile --quiet" );
+		assertEquals( "outfile", terminal.opt.filename );
+		assertEquals( true, terminal.opt.quiet );
+		assertEquals( null, terminal.opt.outfile );
+		
+		
+	//	var opts = opt.Parser.get( "-f -o -q", "foq"  );
+	//	trace(opts);
+		
+		/*
+		terminal = new opt.Parser( "TestOptParser.n" );
+		terminal.addSwitch( ["f","ff","fff"], store( "filename", TString ), "TEST info" );
+		terminal.addSwitch( ["s"], store( "filename", TString ) );
+		terminal.exe = "neko";
+		terminal.description = "This is a testunit for opt.Parser.";
+		terminal.help();
+		*/
+		
+		trace( terminal.opt );
+	}
+	
+	function myCallback() {
+	}
+	
+	static function main() {
+		var r = new haxe.unit.TestRunner();
+		r.add( new TestOptParser() );
+		r.run();
+		
+	}
+	
+}
